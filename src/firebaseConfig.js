84dd600a9d94b05d24d9f7936b6ce59ee8bbdb9a
apena124 <73714064+apena124@users.noreply.github.com>
2022-02-28 16:14:58 -0600

    Login Firebase Done

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBYq5TVz496_9O2Sdy0lxFSMp3IH8sDiTE",
+  authDomain: "vue-3-login-test-ee8c5.firebaseapp.com",
+  projectId: "vue-3-login-test-ee8c5",
+  storageBucket: "vue-3-login-test-ee8c5.appspot.com",
+  messagingSenderId: "534121482502",
+  appId: "1:534121482502:web:31d6872c0156af7cf43687",
+};
